472633c773278d2d12bf87709491a019ed136052
ssda332 <tititiqkfk3@gmail.com>
2024-06-24 00:51:43 +0900

    init

---
+++ src/main/java/hhplus/lectures/Hhplus2Application.java
@@ -0,0 +1,13 @@
+package hhplus.lectures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hhplus2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hhplus2Application.class, args);
+    }
+
+}
---
+++ src/test/java/hhplus/lectures/Hhplus2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hhplus.lectures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hhplus2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
